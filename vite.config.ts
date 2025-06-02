d17dc986c0e2f67e10a6f0b290e45bbdeb05ddd0
destiny19900 <iduozeeosarugue027@gmail.com>
2025-06-02 09:40:15 -0400

    Fix deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://destiny19900.github.io/paradise-nursery/',
+  base: '/paradise-nursery/',
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  }
 })
